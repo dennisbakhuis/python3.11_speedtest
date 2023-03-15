38b76cd76f3ec131b78e2ce96cd4e793d3d2b0f0
Yorksh1re <ezreeeeal@gmail.com>
2023-03-15 10:07:00 +0800

    feat: add PyPy 3.9

--- pi_estimates/Python/run_main_test.py
+++ pi_estimates/Python/run_main_test.py
@@ -18,8 +18,8 @@ TEST_IMAGES = [
     {'name': 'Python 3.8', 'image': 'python:3.8-slim'},
     {'name': 'Python 3.9', 'image': 'python:3.9-slim'},
     {'name': 'Python 3.10', 'image': 'python:3.10-slim'},
-    {'name': 'Python 3.11', 'image': 'python:3.10-slim'},
-    {'name': 'Python 3.11', 'image': 'python:3.11-slim'}
+    {'name': 'Python 3.11', 'image': 'python:3.11-slim'},
+    {'name': 'PyPy 3.9', 'image': 'pypy:3.9-slim'},
 ]
 
 N_POINTS = 10_000_000  # points used to estimate Pi.
@@ -51,21 +51,21 @@ def test_version(image: str) -> float:
         runtime in seconds per test loop.
     """
     output = subprocess.run([
-            'docker',
-            'run',
-            '-it',
-            '--rm',
-            '-v',
-            f'{cwd}/{SCRIPT}:/{SCRIPT}',
-            image,
-            'python',
-            f'/{SCRIPT}',
-            '--n_points',
-            str(N_POINTS),
-            '--n_repeats',
-            str(N_REPEATS),
-            '--only-time',
-        ],
+        'docker',
+        'run',
+        '-it',
+        '--rm',
+        '-v',
+        f'{cwd}/{SCRIPT}:/{SCRIPT}',
+        image,
+        'python',
+        f'/{SCRIPT}',
+        '--n_points',
+        str(N_POINTS),
+        '--n_repeats',
+        str(N_REPEATS),
+        '--only-time',
+    ],
         capture_output=True,
         text=True,
     )
@@ -86,6 +86,3 @@ for item in TEST_IMAGES:
         f"{item['name']} took {ttime} seconds per run."
         f"({NEW_IMAGE['name']} is {(ttime / base_time) - 1:.1%} faster)"
     )
-
-
-
